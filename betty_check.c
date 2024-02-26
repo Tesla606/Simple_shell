0c91571641874d9e787d48bbb01db04261205fd3
Hooley Mass <hockleymasese3@gmail.com>
2024-02-27 01:20:19 +0300

    betty checked 😂

---
+++ betty_check.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A program that passes Betty.
+ *              Calls the print_name function to print "Tesla" in a fun way.
+ *
+ * Return: 0 on success
+ */
+
+void print_name(void)
+{
+    printf("Teeessssllllaaaa!!!\n");
+}
+
+int main(void)
+{
+    print_name();
+    return (0);
+}
