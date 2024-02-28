c720590b565f078c51103a21fe03b83fd9378d46
Hooley Mass <hockleymasese3@gmail.com>
2024-02-29 01:52:29 +0300

    never done ✔️  ✅

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 0x16. C - Simple Shell
 `C` | `Group project` | `Syscall`
 
+![shell](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-low_level_programming/235/shell.jpeg)
+
 This is A simple UNIX command interpreter.
 
 ## List of allowed functions and system calls
--- builtin_commands_extended.c
+++ builtin_commands_extended.c
@@ -3,6 +3,7 @@
 /**
  * _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
+ * @tesla606 - author of file
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
